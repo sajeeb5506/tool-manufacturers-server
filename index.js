9f02c49c00c2341afd3a21ca818f52f7cc138341
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-05-25 14:33:29 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config();
@@ -19,6 +20,7 @@ async function run(){
         const allProducts =client.db("manufacturers").collection("products");
         const allusers =client.db("manufacturers").collection("users");
         const allreviews =client.db("manufacturers").collection("reviews");
+        const  usersinfoLogin =client.db("manufacturers").collection("usersinfo");
 
         //get all products
         app.get('/products', async(req,res)=>{
@@ -45,7 +47,57 @@ async function run(){
         }
         const result= await allusers.insertOne(data);
         return res.send({success:true,result});
-        
+
+    })
+    // login user get
+        app.get ('/user' ,async(req,res)=>{
+          const users = await usersinfoLogin.find().toArray();
+          res.send(users);
+        })
+    // user info when user login,signup
+    app.put('/user/:email',async(req,res)=>{
+      const email= req.params.email;
+      const user = req.body;
+      const filter = {email:email};
+      const options = {upsert:true};
+      const updateDoc = {
+        $set: user,
+      }
+      const result = await usersinfoLogin.updateOne(filter,updateDoc,options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN,{expiresIn:'2h'});
+
+      res.send({result, token});
+    })
+    //get user  use email
+    
+    app.get('/singleuser', async(req,res)=>{
+      const email = req.query.userEmail;
+       const query={email};
+
+      const cursor = allusers.find(query);
+      const singleuser = await cursor.toArray();
+      res.send(singleuser);
+
+       });
+
+      //  update api
+      app.put('/users/:id',async(req,res)=>{
+        const id = req.params.id;
+        console.log(id)
+        const data= req.body
+        console.log(data)
+        const query = {_id:ObjectId(id)};
+        const options = {upsert:true}
+        const update = {
+            $set:{
+              
+                users:data
+
+            },
+        };
+        const result= await allusers.updateMany(query,update,options);
+        res.send(result);
+
     })
     // review add api 
     app.post('/reviews',async(req,res)=>{
